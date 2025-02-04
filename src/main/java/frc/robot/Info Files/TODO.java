484ce99ad65b477d7cbdf7a333550f51302c1d47
price7000 <114043330+price7000@users.noreply.github.com>
2025-02-04 05:58:20 +0000

    Added TODO Stuff

---
+++ src/main/java/frc/robot/Info Files/TODO.java
@@ -0,0 +1,75 @@
+/*
+* THIS IS USED TO IMPROVE TODO STATEMENTS
+*/
+
+/********** HOW TO USE **********/
+/*
+ * To use one of the keywords, start a comment, and then type them exactly
+ *      NOTE: If you don't write them exactly, then they wont format properly. They are case sensitive
+ *  Example:
+ *     // REVIEW: Review This
+ *  Extension link:
+ *     https://marketplace.visualstudio.com/items?itemName=jgclark.vscode-todo-highlight
+ * 
+ */
+
+/********** SETUP **********/
+/*
+ * 1. Install the TODO Highlight Extension:
+ *      https://marketplace.visualstudio.com/items?itemName=jgclark.vscode-todo-highlight
+ *      - It can also be installed through the VS Code marketplace
+ * 2. Open VS Code settings.json
+ *      1. Go to the Command Pallet (Hit F1, or the WPI Icon (Same button to deploy code))
+ *      2. Enter the following in the Command Pallet, and hit ENTER:
+ *           >Preferences:Open User Settings (JSON)
+ *      3. Delete, everything from settings.json, and paste everything from TODO-JSON-ASDD.json
+ *           into settings.json
+ *           3. ALTERNATE:
+ *                You can do the above, but if there is anything in your settings.json that is required
+ *                   for your VS Code, then just paste from where it says "START": "START" to where
+ *                   it says "END": "END" into your settings.json
+ *  
+ * 
+ */
+
+/********** RECOGNISED KEYWORDS **********/
+/*
+ * Review:
+ *    REVIEW:
+ *    Used when a piece of code needs to be reviewed
+ * 
+ * To Do:
+ *    TODO:
+ *    Should be put in place when something needs to be done
+ * 
+ * Info:
+ *    INFO:
+ *    Should be used when there is a piece of information that needs to stand out
+ * 
+ * Note:
+ *    NOTE:
+ *    Same use as Info:
+ * 
+ * Temp:
+ *    TEMP:
+ *    Used to mark a temporary piece of code
+ * 
+ * Fix Me:
+ *    FIXME:
+ *    Marks a piece of code that dosen't work correctly. It should be followed with what needs
+ *       needs to be fixed, and any other relavent info (multiple lines can be used. Just use Fixme
+ *       as a beginning marker to an issue)
+ * 
+ * Important:
+ *    IMPORTANT:
+ *    Should be used only where it is required
+ *    It should mark something that is imperitave that the coder see it
+ * 
+ * Seperator:
+ *    SEPERATOR:
+ *    Can be used to seperate or mark sections of text
+ *    (Unsure if this will be useful, but I put it in anyway)
+ * 
+ */
+
+
