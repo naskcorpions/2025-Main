0d3bd4b7ea276c7636f9c42512b9265aea6c00e8
price7000 <114043330+price7000@users.noreply.github.com>
2025-02-04 03:44:17 +0000

    Added Motors info file, and info to Dashboard.java

--- src/main/java/frc/robot/subsystems/Dashboard.java
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -10,6 +10,35 @@ public class Dashboard extends SubsystemBase {
     @Override
     public void periodic() {
         SmartDashboard.putNumber("Batt Volts", RobotController.getBatteryVoltage());
+
+        // TODO: ADD?
+        /*
+         * Things to possibly add:
+         *    SWERVE: 
+         *     - Module Teletry (Each module orientation)
+         *     - Chassis Data (Robot overall orientation)
+         *    ELEVATOR:
+         *     - Position / Level
+         *    VISION:
+         *     - Camera Data:
+         *         - Yaw
+         *         - Tag Number
+         *         - Tag Distance
+         *         - Is in Range/Is alligned?
+         *    AUTO:
+         *     - Auto Selector
+         *    OTHER:
+         *     Batt Voltage (Bar or Graph)
+         *     Motor Data:
+         *       - Temp
+         *           * Could have as a number in the dashboard,
+         *           *   or it could be sent as a notification
+         *     Other Notifications
+         *         * Could use other notificaitons related to overall robot health,
+         *         *   or any errors
+         *     Match Info
+         * 
+         */
     }
 
 }
